41c1e30de80ce7d7afc691b6929d83f1c1ea4c42
Tim Horton <hortont424@gmail.com>
2009-05-21 07:50:24 -0400

    Macports setup script from building introspection

---
+++ setup_macports_environment.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=/opt/local/lib/pkgconfig:$PKG_CONFIGPATH
+export ACLOCAL_FLAGS="-I /opt/local/share/aclocal/"
+export PATH=/opt/local/bin:$PATH
