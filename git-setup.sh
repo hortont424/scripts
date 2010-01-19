e1a0b28808201bf17db60a916a1cca2b3b6c54eb
Tim Horton <hortont424@gmail.com>
2010-01-18 19:37:14 -0500

    add github.user to gitconfig. don't forget to set github.token, too.

--- git-setup.sh
+++ git-setup.sh
@@ -2,3 +2,4 @@ git config --global user.name "Tim Horton"
 git config --global user.email "hortont424@gmail.com"
 git config --global color.ui auto
 git config --global push.default current
+git config --global github.user hortont424
