d66545ea2bce80e98889d530202a767cf3afa9a8
Tim Horton <hortont424@gmail.com>
2010-01-18 19:17:03 -0500

    Minimalize /srv/share/public diffs

--- email-share-changes.sh
+++ email-share-changes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 find /srv/share/public 2>&1 | sort > /tmp/.share.files.2
-diff /tmp/.share.files.1 /tmp/.share.files.2 | sed -e 's/^>/+/' -e 's/^</-/' | mail -E hortont
+diff --suppress-common-lines --minimal /tmp/.share.files.1 /tmp/.share.files.2 | sed -e 's/^>/+/' -e 's/^</-/' | mail -s "/srv/share/public changes" -E hortont
 mv /tmp/.share.files.2 /tmp/.share.files.1
