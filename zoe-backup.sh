6e0a40a7d96ad04701f5dd47e6c76e3a20ea9001
Tim Horton <hortont424@gmail.com>
2010-08-29 19:19:49 -0400

    working on zoe backup script

--- zoe-backup.sh
+++ zoe-backup.sh
@@ -1,7 +1,8 @@
 rsync -rlDczPv --delete-after ~/Desktop jayne.local:/srv/share/private/hortont/Backups/Zoe/
 rsync -rlDczPv --delete-after ~/Pictures/ jayne.local:/srv/share/public/Pictures/
 rsync -rlDczPv --delete-after ~/Documents jayne.local:/srv/share/private/hortont/Backups/Zoe/
-rsync -rlDczPv --delete-after ~/Music/iTunes/iTunes\ Music/Music/ jayne.local:/srv/share/public/Music/
+rsync -rlDczPv --delete-after /Volumes/MCP/Music/iTunes/iTunes\ Music/Music/ jayne.local:/srv/share/public/Music/
+rsync -rlDczPv --delete-after /Volumes/MCP/Music --exclude="iTunes/iTunes Music/Music/" jayne.local:/srv/share/private/hortont/Backups/Zoe/Music/
 rsync -rlDczPv --delete-after /Library/Fonts jayne.local:/srv/share/private/hortont/Backups/Zoe/
-rsync -rlDczPv --delete-after "/Users/hortont/Library/Application Support/Adium 2.0/" jayne.local:/srv/share/private/hortont/Backups/Kaylee/Logs/
+rsync -rlDczPv --delete-after "/Users/hortont/Library/Application Support/Adium 2.0/" jayne.local:/srv/share/private/hortont/Backups/Zoe/Logs/
 #ssh jayne.local perl /srv/share/www/aperture/aperture.pl
